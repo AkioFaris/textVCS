dd891b6bb45c2b605f2b7cb05784dc5494fd6665
AkioFaris <rnanimo@gmail.com>
2015-11-08 21:24:01 +0300

    troubles with streams

--- src/main/java/CommandLineParser.java
+++ src/main/java/CommandLineParser.java
@@ -2,41 +2,69 @@ import org.apache.commons.cli2.*;
 import org.apache.commons.cli2.builder.*;
 import org.apache.commons.cli2.commandline.Parser;
 import org.apache.commons.cli2.util.HelpFormatter;
+import java.io.*;
 
 /**
  * Created by Akio on 12/10/2015.
  */
 public class CommandLineParser {
-  public static void main(String[] args) throws OptionException {
-    final DefaultOptionBuilder oBuilder = new DefaultOptionBuilder();
-    final ArgumentBuilder aBuilder = new ArgumentBuilder();
-    final GroupBuilder gBuilder = new GroupBuilder();
+  public static final DefaultOptionBuilder oBuilder;
+  public static final ArgumentBuilder aBuilder;
+  public static final GroupBuilder gBuilder;
 
-    final Option file =
+  public static final Option file;
+  public static final Option inputFile;
+  public static final Option outputFile;
+  public static final Option sourceFile;
+  public static final Option revision;
+
+  public static final Option help;
+  public static final Option create;
+  public static final Option update;
+  public static final Option get;
+
+  public static final Group commandOptions;
+
+  public static Parser parser;
+  public static CommandLine cl;
+
+  public static String iFilename = null;
+  public static String oFilename = null;
+  public static String sFilename = null;
+  public static String rIndex = null;
+
+  public static InputStream inStream;
+  public static OutputStream outStream;
+
+  static {
+    oBuilder = new DefaultOptionBuilder();
+    aBuilder = new ArgumentBuilder();
+    gBuilder = new GroupBuilder();
+    file =
             aBuilder
                     .withName("file")
                     .withMinimum(1)
                     .withMaximum(1)
                     .create();
-    final Option inputFile =
+    inputFile =
             oBuilder
                     .withShortName("i")
                     .withDescription("use given *.txt file as an input")
                     .withArgument((Argument) file)
                     .create();
-    final Option outputFile =
+    outputFile =
             oBuilder
                     .withShortName("o")
                     .withDescription("create an output file with this filename")
                     .withArgument((Argument) file)
                     .create();
-    final Option sourceFile =
+    sourceFile =
             oBuilder
                     .withShortName("s")
                     .withDescription("use given *.vc file as a source")
                     .withArgument((Argument) file)
                     .create();
-    final Option revision =
+    revision =
             oBuilder
                     .withShortName("r")
                     .withDescription("set revision message")
@@ -47,13 +75,12 @@ public class CommandLineParser {
                                     .withMaximum(1)
                                     .create())
                     .create();
-
-    final Option help =
+    help =
             oBuilder
                     .withLongName("help")
                     .withDescription("print help")
                     .create();
-    final Option create =
+    create =
             oBuilder
                     .withLongName("create")
                     .withDescription("create a *.vc file")
@@ -67,21 +94,21 @@ public class CommandLineParser {
                                     .create()
                     )
                     .create();
-    final Option update =
+    update =
             oBuilder
                     .withLongName("update")
                     .withDescription("update the *.vc file")
                     .withChildren(
                             gBuilder
                                     .withOption(inputFile)
-                                    .withOption(sourceFile)
+                                    .withOption(outputFile)
                                     .withOption(revision)
                                     .withMinimum(3)
                                     .withMaximum(3)
                                     .create()
                     )
                     .create();
-    final Option get =
+    get =
             oBuilder
                     .withLongName("get")
                     .withDescription("get a *.txt file from the *.vc file")
@@ -95,8 +122,7 @@ public class CommandLineParser {
                                     .create()
                     )
                     .create();
-
-    final Group commandOptions =
+    commandOptions =
             gBuilder
                     .withName("commandOptions")
                     .withOption(help)
@@ -106,50 +132,49 @@ public class CommandLineParser {
                     .withMinimum(1)
                     .withMaximum(1)
                     .create();
+    parser = new Parser();
+  }
 
-    Parser parser = new Parser();
+
+  public static void main(String[] args) throws OptionException, IOException {
     parser.setGroup(commandOptions);
-    CommandLine cl = parser.parse(args);
-
-    String iFilename = null;
-    String oFilename = null;
-    String sFilename = null;
-    String rIndex = null;
-
-    if (cl.hasOption(help)) {
-      displayHelp(commandOptions);
-      return;
-    }
+    cl = parser.parse(args);
 
     if (cl.hasOption(inputFile)) {
       iFilename = (String) cl.getValue(inputFile);
+      inStream = new FileInputStream(new File(iFilename));
     }
     if (cl.hasOption(outputFile)) {
       oFilename = (String) cl.getValue(outputFile);
+      outStream = new FileOutputStream(new File(oFilename));
     }
     if (cl.hasOption(sourceFile)) {
       sFilename = (String) cl.getValue(sourceFile);
+      inStream = new FileInputStream(new File(sFilename));
     }
     if (cl.hasOption(revision)) {
       rIndex = (String) cl.getValue(revision);
     }
 
-    if (cl.hasOption(create) && iFilename != null && oFilename != null && rIndex != null) {
-      VCS.createVCFile(iFilename, oFilename, rIndex);
+    if (cl.hasOption(help)) {
+      displayHelp(commandOptions);
+    } else if (cl.hasOption(create) && iFilename != null && oFilename != null && rIndex != null) {
+      VCS.createVCFile(inStream, outStream, rIndex);
       System.out.println("CREATE I: " + iFilename + " O: " + oFilename + " R: " + rIndex); // for debugging
-    }
-    if (cl.hasOption(update) && iFilename != null && sFilename != null && rIndex != null) {
-      VCS.update(iFilename, sFilename, rIndex);
+    } else if (cl.hasOption(update) && iFilename != null && oFilename != null && rIndex != null) {
+      VCS.update(inStream, outStream, rIndex);
       System.out.println("UPDATE I: " + iFilename + " S: " + sFilename + " R: " + rIndex); // for debugging
-    }
-    if (cl.hasOption(get) && sFilename != null && oFilename != null && rIndex != null) {
-      VCS.getFileVersion(sFilename, oFilename, rIndex);
+    } else if (cl.hasOption(get) && sFilename != null && oFilename != null && rIndex != null) {
+      VCS.getFileVersion(inStream, outStream, rIndex);
       System.out.println("GET S: " + sFilename + " O: " + oFilename + " R: " + rIndex); // for debugging
+    } else {
+      System.out.println("Input was not recognized.");
+      displayHelp(commandOptions);
     }
   }
 
   private static void displayHelp(final Group options) {
-    HelpFormatter hf = new HelpFormatter();
+    final HelpFormatter hf = new HelpFormatter();
     hf.setShellCommand("textVCS");
     hf.setGroup(options);
     hf.getFullUsageSettings().add(DisplaySetting.DISPLAY_GROUP_NAME);
--- src/main/java/VCS.java
+++ src/main/java/VCS.java
@@ -1,75 +1,86 @@
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 /**
  * Created by Akio on 10/10/2015.
  */
 public class VCS {
+  private static final String LINE_SEPARATOR = "\n";
 
-  public static void createVCFile(final String inputFilename, final String outputFilename,
-                                  final String revision) {
-    final File newFile;
-    try {
-      newFile = new File(outputFilename);
 
-      if (!newFile.exists()) {
-        if (!newFile.createNewFile()) {
-          throw new IOException("Unable to create file: " + outputFilename);
-        }
+  public static void createVCFile(final InputStream inStream, final OutputStream outStream, final String revision) throws IOException {
+    try (
+            final BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+            final OutputStreamWriter outW = new OutputStreamWriter(outStream);
+            final PipedOutputStream pout = new PipedOutputStream();
+            final PipedInputStream pin = new PipedInputStream(pout)
+    ) {
+      final byte[] buffer = new byte[1024];
+      while (inStream.read(buffer) > -1) {
+        pout.write(buffer);
+       // System.out.println(buffer.toString()); //tmp
       }
+     // System.out.println("I'm here! (1)"); //tmp
+      final String versionHeader = "r" + revision + LINE_SEPARATOR// + Objects.toString(getNumberOfLines(pin))
+              + " operations";
+      outW.write(versionHeader);
+      //System.out.println("I'm here! (2)"); //tmp
 
-      final FileWriter fw = new FileWriter(outputFilename);
-      final FileInputStream fIn = new FileInputStream(inputFilename);
-      final BufferedReader br = new BufferedReader(new InputStreamReader(fIn));
-      int nLines = 0;
-
-      while (br.readLine() != null) {
-        ++nLines;
-      }
-      final String versionHeader = "r" + revision + System.getProperty("line.separator")
-              + Objects.toString(nLines) + " operations";
-      fw.write(versionHeader);
-
-      fIn.getChannel().position(0);
       String line;
       while ((line = br.readLine()) != null) {
-        fw.append(System.getProperty("line.separator"));
-        fw.append(line);
+        outW.write(LINE_SEPARATOR + line);
+      //  System.out.println(LINE_SEPARATOR + line); //tmp
       }
+      //outW.flush();
+    //  System.out.println("I'm here! (3)"); //tmp
 
-      br.close();
-      fw.close();
-      fIn.close();
-    } catch (Exception e) {
-      System.err.println("Exception occurred: " + e.toString());
-      e.printStackTrace();
     }
   }
 
-  public static void update(final String inputFilename, final String sourceFilename, final String revision) {
-    final int prevRevision;
-    final int currevision;
-    final Integer[] lineLCSindex = null;
-    final int LCSlen;
+  public static void update(final InputStream inStream, final OutputStream outStream, final String revision) throws IOException {
+    String prevRevision = null;
+    //final String currRevision;
+    final Integer[] lineLCSIndex = null;
+    final int LCSLen;
+    try (
+            final BufferedReader br = new BufferedReader(new InputStreamReader(inStream))) {/*get last revision message*/
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.startsWith("r")) {
+          prevRevision = line.substring(1);
+        }
+      }
+
+//      /*get temporary file (last revision version)*/
+//      final File tempFile = new File(inFile.getAbsolutePath() + ".prevVersion");
+//      getFileVersion(sourceFilename, tempFile.getPath(), prevRevision);
+//      if (!tempFile.exists()) {
+//        if (!tempFile.createNewFile()) {
+//          throw new IOException("Unable to create tmp file: " + tempFile.getPath());
+//        }
+//      }
+//
+//      ArrayList<Integer> commonLines = LCS(inFile, tempFile);
+//
+//      if (!tempFile.delete()) {
+//        throw new IOException("Unable to delete file " + tempFile.getPath());
+//      }
+    }
   }
 
 
-  public static void getFileVersion(final String sourceFilename, final String outputFilename, final String revision) {
-    final File newFile;
-    try {
-      newFile = new File(outputFilename);
+  public static void getFileVersion(final InputStream sourceStream, final OutputStream outStream, final String revision) throws IOException {
+    try (
+            final BufferedReader br = new BufferedReader(new InputStreamReader(sourceStream));
+            final PipedOutputStream pout = new PipedOutputStream();
+            final PipedInputStream pin = new PipedInputStream(pout);
+            final OutputStreamWriter outW = new OutputStreamWriter(outStream)) {
+      new PrintWriter(outStream).close(); /*clear the file's content*/
 
-      if (!newFile.exists()) {
-        if (!newFile.createNewFile()) {
-          throw new IOException("Unable to create file: " + outputFilename);
-        }
-      } else {
-        new PrintWriter(outputFilename).close(); /*clear the file's content*/
-      }
-
-      final BufferedReader br = new BufferedReader(new FileReader(sourceFilename));
       String line;
       boolean isRequestedVersion = false;
 
@@ -80,7 +91,7 @@ public class VCS {
         }
         line = br.readLine();
         final int nLines = getReadNumberOfLines(line);
-        addLinesToFile(br, newFile, 0, nLines);
+        addLinesToStream(br, pin, pout, 0, nLines);
       }
 
       while ((((line = br.readLine())) != null) && !isRequestedVersion) {
@@ -95,98 +106,176 @@ public class VCS {
           final Integer newFileLinesInd = Integer.parseInt(numbers[0].replaceAll("[^0-9]", ""));
           final Integer newFileLinesNum = Integer.parseInt(numbers[1]);
           if (line.startsWith("-")) {
-            removeLinesFromFile(newFile, newFileLinesInd, newFileLinesNum);
+            removeLinesFromStream(pin, pout, newFileLinesInd, newFileLinesNum);
           }
           if (line.startsWith("+")) {
-            addLinesToFile(br, newFile, newFileLinesInd, newFileLinesNum);
+            addLinesToStream(br, pin, pout, newFileLinesInd, newFileLinesNum);
           }
         }
       }
-      br.close();
-    } catch (Exception e) {
-      System.err.println("Exception occurred: " + e.toString());
-      e.printStackTrace();
+
+      final byte[] buffer = new byte[1024];
+      while (pin.read(buffer) > -1) {
+        outW.write(buffer.toString());
+      }
     }
   }
 
-  private static void LCS() /*the longest common subsequence*/ {
+  private static int getNumberOfLines(final PipedInputStream pInStream) throws IOException {
+    int nLines = 0;
+    try (final BufferedReader br = new BufferedReader(new InputStreamReader(pInStream))) {
+      while (br.readLine() != null) {
+        ++nLines;
+      }
+    }
+    return nLines;
+  }
 
+  private static int getLinePos(final PipedInputStream pInStream, final int lineInd) throws IOException {// returns the position of lineInd's line in file
+    int linePos = 0;
+    if (lineInd == 0) // tmp??
+      return 0; // tmp??
+    try (final BufferedReader br = new BufferedReader(new InputStreamReader(pInStream))) {
+      int r;
+      int nLineSeparator = 0;
+      while ((r = br.read()) != -1 && nLineSeparator < lineInd - 1) {
+        final char ch = (char) r;
+        if (ch == '\n') {
+          ++nLineSeparator;
+        }
+        ++linePos;
+      }
+    }
+    return linePos;
+  }
+
+
+  private static ArrayList backtrack(final int[][] lcsLen,
+                                     final PipedInputStream pInStream1, final PipedInputStream pInStream2,
+                                     final int i, final int j, ArrayList bt) throws IOException {
+//    if (i == 0 || j == 0) {
+//      return bt;
+//    }
+//    final RandomAccessFile raf1 = new RandomAccessFile(pInStream1, "rw");
+//    final RandomAccessFile raf2 = new RandomAccessFile(pInStream2, "rw");
+//    raf1.seek(getLinePos(pInStream1, i));
+//    raf2.seek(getLinePos(pInStream2, j));
+//    final String f1Line = raf1.readLine();
+//    final String f2Line = raf2.readLine();
+//    raf1.close();
+//    raf2.close();
+//    if (f1Line.equals(f2Line)) {
+//      bt.add(i);
+//      return backtrack(lcsLen, pInStream1, pInStream2, i - 1, j - 1, bt);
+//    } else {
+//      if (lcsLen[i][j - 1] > lcsLen[i - 1][j]) {
+        return backtrack(lcsLen, pInStream1, pInStream2, i, j - 1, bt);
+//      } else {
+//        return backtrack(lcsLen, pInStream1, pInStream2, i - 1, j, bt);
+//      }
+//    }
+  }
+
+  private static ArrayList LCS(final PipedInputStream pInStream1, final PipedInputStream pInStream2) throws IOException {/*the longest common subsequence*/
+    ArrayList<Integer> commonLines = new ArrayList<>();
+
+    try (
+            final BufferedReader br1 = new BufferedReader(new InputStreamReader(pInStream1));
+            final BufferedReader br2 = new BufferedReader(new InputStreamReader(pInStream2))
+    ) {
+      final int file1LinesNum = getNumberOfLines(pInStream1);
+      final int file2LinesNum = getNumberOfLines(pInStream2);
+      final int[][] lcsLen = new int[file1LinesNum][file2LinesNum];
+      for (int i = 0; i < file1LinesNum; i++) {
+        lcsLen[i][0] = 0;
+      }
+      for (int j = 0; j < file2LinesNum; j++) {
+        lcsLen[0][j] = 0;
+      }
+
+      for (int i = 1; i < file1LinesNum; i++) {
+        final String f1Line = br1.readLine();
+        for (int j = 1; j < file2LinesNum; j++) {
+          final String f2Line = br2.readLine();
+          if (f1Line.equals(f2Line)) {
+            lcsLen[i][j] = lcsLen[i - 1][j - 1] + 1;
+          } else {
+            lcsLen[i][j] = Math.max(lcsLen[i][j - 1], lcsLen[i - 1][j]);
+          }
+        }
+      }
+      commonLines = backtrack(lcsLen, pInStream1, pInStream2, file1LinesNum, file2LinesNum, commonLines);
+    }
+    return commonLines;
   }
 
   private static int getReadNumberOfLines(final String line) {
     return Integer.parseInt(line.replaceAll(" operations(.*)", ""));
   }
 
-  private static void addLinesToFile(final BufferedReader br, final File file, final int linesInd, final int linesNum)
-          throws Exception {
-    if (!file.exists()) {
-      return;
-    }
-    final File tempFile = new File(file.getAbsolutePath() + ".tmp");
-    if (!tempFile.exists()) {
-      if (!tempFile.createNewFile()) {
-        throw new IOException("Unable to create tmp file: " + tempFile.getPath());
+  private static void addLinesToStream(final BufferedReader br, final PipedInputStream pInStream, final PipedOutputStream pOutStream,
+                                       final int linesInd, final int linesNum)
+          throws IOException
+  {
+    try (final ByteArrayOutputStream arrOut = new ByteArrayOutputStream()) {
+      final byte[] buffer = new byte[1024];
+      int len;
+      while ((len = pInStream.read(buffer)) > -1) {
+        arrOut.write(buffer, 0, len);
       }
-    }
-    FileUtils.copyFile(file, tempFile);
+      arrOut.flush();
+      pOutStream.flush();
 
-    final BufferedReader tmpBr = new BufferedReader(new FileReader(tempFile));
-    final FileWriter fw = new FileWriter(file.getPath(), false);
+      try (
+              final InputStream inStream = new ByteArrayInputStream(arrOut.toByteArray());
+              final BufferedReader tmpBr = new BufferedReader(new InputStreamReader(inStream));
+              final OutputStreamWriter outW = new OutputStreamWriter(pOutStream)
+      ) {
+        int nLines = 0;
+        String line;
+        while (nLines < linesInd) {
+          line = tmpBr.readLine();
+          outW.write(line + LINE_SEPARATOR);
+          ++nLines;
+        }
 
-    int nLines = 0;
-    String line;
-    while (nLines < linesInd) {
-      line = tmpBr.readLine();
-      fw.append(line).append(System.getProperty("line.separator"));
-      ++nLines;
-    }
-
-    for (int j = 0; j < linesNum; j++) {
-      line = br.readLine();
-      fw.append(line + System.getProperty("line.separator"));
-    }
-    while ((line = tmpBr.readLine()) != null) {
-      fw.append(line).append(System.getProperty("line.separator"));
-    }
-
-    fw.close();
-    tmpBr.close();
-    if (!tempFile.delete()) {
-      throw new Exception("Unable to delete file " + tempFile.getPath());
+        for (int j = 0; j < linesNum; j++) {
+          line = br.readLine();
+          outW.write(line + LINE_SEPARATOR);
+        }
+        while ((line = tmpBr.readLine()) != null) {
+          outW.write(line + LINE_SEPARATOR);
+        }
+      }
     }
   }
 
-  private static void removeLinesFromFile(final File file, final int linesInd, final int linesNum)
-          throws Exception {
-    if (!file.exists()) {
-      return;
-    }
-    final File tempFile = new File(file.getAbsolutePath() + ".tmp");
-    if (!tempFile.exists()) {
-      if (!tempFile.createNewFile()) {
-        throw new IOException("Unable to create tmp file: " + tempFile.getPath());
+  private static void removeLinesFromStream(final PipedInputStream pInStream, final PipedOutputStream pOutStream,
+                                            final int linesInd, final int linesNum) throws IOException {
+    try (final ByteArrayOutputStream arrOut = new ByteArrayOutputStream()) {
+      final byte[] buffer = new byte[1024];
+      int len;
+      while ((len = pInStream.read(buffer)) > -1) {
+        arrOut.write(buffer, 0, len);
       }
-    }
-    FileUtils.copyFile(file, tempFile);
+      arrOut.flush();
+      pOutStream.flush();
 
-    final BufferedReader br = new BufferedReader(new FileReader(tempFile));
-    final FileWriter fw = new FileWriter(file.getPath(), false);
+      try (
+              final InputStream inStream = new ByteArrayInputStream(arrOut.toByteArray());
+              final BufferedReader br = new BufferedReader(new InputStreamReader(inStream));
+              final OutputStreamWriter outW = new OutputStreamWriter(pOutStream)
+      ) {
+        int nLines = 0;
+        String line;
 
-    int nLines = 0;
-    String line;
-
-    while ((line = br.readLine()) != null) {
-      if (linesInd + linesNum <= nLines || nLines < linesInd) {
-        fw.write(line);
-        fw.append(System.getProperty("line.separator"));
+        while ((line = br.readLine()) != null) {
+          if (linesInd + linesNum <= nLines || nLines < linesInd) {
+            outW.write(line + LINE_SEPARATOR);
+          }
+          ++nLines;
+        }
       }
-      ++nLines;
-    }
-
-    fw.close();
-    br.close();
-    if (!tempFile.delete()) {
-      throw new Exception("Unable to delete file " + tempFile.getPath());
     }
   }
 }
